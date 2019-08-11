29e5f2039d72bd5e9927d56934fa020e1b9eeb42
Saba Imran <sabaimran48@gmail.com>
2019-08-11 12:10:06 -0700

    Trying to fix compileissue

--- joyride/client/index.jsx
+++ joyride/client/index.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import ReactDOM from "react-dom";
 import App from './components/App.jsx';
 import './css/index.css';
 
-render(
-  <App />
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
